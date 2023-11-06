18449b7fe4fb78ece3168e6674ccc425cca43737
krishna8452 <krishnachaudhary8452@gmail.com>
2023-11-06 14:26:36 +0545

    sales analytics added

--- modules/order/orderController.js
+++ modules/order/orderController.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const pool = require("../../db/db");
 
 exports.addToCart = async (req, res) => {
@@ -63,14 +64,16 @@ exports.addToCart = async (req, res) => {
 
       const updatedTotalPriceQuery =
         "SELECT SUM(price) AS total_price FROM carts_products WHERE cart_id = $1";
-      const updatedTotalPriceResult = await pool.query(updatedTotalPriceQuery, [cartId]);
+      const updatedTotalPriceResult = await pool.query(updatedTotalPriceQuery, [
+        cartId,
+      ]);
 
       const updatedTotalPrice = updatedTotalPriceResult.rows[0].total_price;
 
       const updateCartQuery = "UPDATE carts SET total_price = $1 WHERE id = $2";
       await pool.query(updateCartQuery, [updatedTotalPrice, cartId]);
 
-      res.send("Cart updated successfully");
+      res.status(200).json({success:"Cart updated successfully",});
     } else {
       let total_price = 0;
 
@@ -104,18 +107,17 @@ exports.addToCart = async (req, res) => {
         );
       }
 
-      await pool.query(
-        "UPDATE carts SET total_price = $1 WHERE id = $2",
-        [total_price, insertCartResult.rows[0].id]
-      );
+      await pool.query("UPDATE carts SET total_price = $1 WHERE id = $2", [
+        total_price,
+        insertCartResult.rows[0].id,
+      ]);
 
-      res.send("New cart created with ID: " + insertCartResult.rows[0].id);
+      res.send("product added to cart successfully: ");
     }
   } catch (error) {
     res.status(500).send("Internal Server Error");
   }
 };
-
 
 exports.removeFromCart = async (req, res) => {
   const userId = parseInt(req.query.userId);
@@ -162,15 +164,323 @@ exports.removeFromCart = async (req, res) => {
 
     const updatedTotalPriceQuery =
       "SELECT SUM(price) AS total_price FROM carts_products WHERE cart_id = $1";
-    const updatedTotalPriceResult = await pool.query(updatedTotalPriceQuery, [cartId]);
+    const updatedTotalPriceResult = await pool.query(updatedTotalPriceQuery, [
+      cartId,
+    ]);
     const updatedTotalPrice = updatedTotalPriceResult.rows[0].total_price;
 
-    await pool.query("UPDATE carts SET total_price = $1 WHERE id = $2", [updatedTotalPrice, cartId]);
+    await pool.query("UPDATE carts SET total_price = $1 WHERE id = $2", [
+      updatedTotalPrice,
+      cartId,
+    ]);
 
     res.send("Product removed from the cart, and cart updated");
   } catch (error) {
     res.status(500).send("Internal Server Error");
   }
 };
+
+exports.checkout = async (req, res) => {
+  const userId = parseInt(req.query.userId);
+  const cartId = parseInt(req.query.cartId);
+  const query = "SELECT * FROM carts WHERE user_id = $1 AND id = $2";
+  const userCartData = await pool.query(query, [userId, cartId]);
+  if (userCartData.rowCount === 0) {
+    return res
+      .status(404)
+      .json({ message: "carts for this user not found !!!" });
+  }
+  const userCart = userCartData.rows[0];
+  const cartProduct = await pool.query(
+    "SELECT cp.quantity, cp.price, p.id, p.name FROM carts_products cp JOIN products p ON CP.product_id = p.id  WHERE cp.cart_id =$1",
+    [userCart.id]
+  );
+  userCart.products = cartProduct.rows;
+  if (userCart.total_price < 500) {
+    return res.status(400).json({
+      message: "The must be minimum of cart price Rs 500 to get checked out",
+    });
+  }
+
+  const order = await pool.query(
+    "INSERT INTO orders (user_id, total_price) VALUES($1, $2) RETURNING *",
+    [userId, userCart.total_price]
+  );
+  const orderId = order.rows[0].id;
+  for (const product of userCart.products) {
+    const productDetails = await pool.query(
+      "SELECT * FROM products WHERE id = $1",
+      [product.id]
+    );
+    if (productDetails.rowCount === 0) {
+      return res.status(400).json({ message: "product not found" });
+    }
+    const productDetailsRow = productDetails.rows[0];
+    if (productDetailsRow) {
+      productDetailsRow.quantity -= product.quantity;
+      if (productDetailsRow.quantity < product.quantity) {
+        res.status(400).json({ message: "product out of stock" });
+      }
+      await pool.query("UPDATE products SET quantity = $1 WHERE id = $2", [
+        productDetailsRow.quantity,
+        product.id,
+      ]);
+      await pool.query(
+        "INSERT INTO orders_products (order_id, product_id, quantity, price) VALUES($1, $2, $3, $4)",
+        [orderId, product.id, product.quantity, product.price]
+      );
+    } else {
+      return res.status(404).json({ message: "product not found" });
+    }
+    await pool.query('DELETE FROM carts_products WHERE cart_id = $1', [cartId]);
+    await pool.query('DELETE FROM carts WHERE user_id = $1', [userId]);
+  }
+  res.status(200).json({userCart, message: "cart checked out successfully" });
+};
+
+exports.getAllOrders = async (req, res) => {
+  try {
+    const query = `
+      SELECT
+        o.id AS order_id,
+        o.user_id,
+        o.total_price AS order_total_price,
+        o.created_at AS order_created_at,
+        op.product_id,
+        op.quantity AS product_quantity,
+        p.name AS product_name,
+        p.price AS product_price
+      FROM orders o
+      JOIN orders_products op ON o.id = op.order_id
+      JOIN products p ON op.product_id = p.id
+    `;
+
+    const result = await pool.query(query);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "No orders found" });
+    }
+
+    const ordersWithProducts = [];
+
+    for (const row of result.rows) {
+      const existingOrder = ordersWithProducts.find((order) => order.order_id === row.order_id);
+
+      if (existingOrder) {
+        existingOrder.products.push({
+          product_id: row.product_id,
+          product_name: row.product_name,
+          product_quantity: row.product_quantity,
+          product_price: row.product_price,
+        });
+      } else {
+        ordersWithProducts.push({
+          order_id: row.order_id,
+          user_id: row.user_id,
+          order_total_price: row.order_total_price,
+          order_created_at: row.order_created_at,
+          products: [
+            {
+              product_id: row.product_id,
+              product_name: row.product_name,
+              product_quantity: row.product_quantity,
+              product_price: row.product_price,
+            },
+          ],
+        });
+      }
+    }
+
+    return res.status(200).json({success:"order list fetched successfully",ordersWithProducts});
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+exports.viewOrderOfUser = async (req, res) => {
+  const userId = parseInt(req.params.id);
+  try {
+    const userExistsResult = await pool.query(
+      "SELECT * FROM users WHERE id = $1",
+      [userId]
+    );
+
+    if (userExistsResult.rowCount === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const query = `
+      SELECT
+        o.id AS order_id,
+        o.user_id,
+        o.total_price AS order_total_price,
+        o.created_at AS order_created_at,
+        op.product_id,
+        op.quantity AS product_quantity,
+        p.name AS product_name,
+        p.price AS product_price
+      FROM orders o
+      JOIN orders_products op ON o.id = op.order_id
+      JOIN products p ON op.product_id = p.id
+      WHERE o.user_id = $1
+    `;
+
+    const result = await pool.query(query, [userId]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "No orders found for the user" });
+    }
+
+    const ordersWithProducts = [];
+
+    for (const row of result.rows) {
+      const existingOrder = ordersWithProducts.find((order) => order.order_id === row.order_id);
+
+      if (existingOrder) {
+        existingOrder.products.push({
+          product_id: row.product_id,
+          product_name: row.product_name,
+          product_quantity: row.product_quantity,
+          product_price: row.product_price,
+        });
+      } else {
+        ordersWithProducts.push({
+          order_id: row.order_id,
+          user_id: row.user_id,
+          order_total_price: row.order_total_price,
+          order_created_at: row.order_created_at,
+          products: [
+            {
+              product_id: row.product_id,
+              product_name: row.product_name,
+              product_quantity: row.product_quantity,
+              product_price: row.product_price,
+            },
+          ],
+        });
+      }
+    }
+
+    return res.status(200).json(ordersWithProducts);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+  res.json(userId)
+};
+
+
+exports.totalRevenue = async (req, res) => {
+  let startDate = req.query.startDate;
+  let endDate = req.query.endDate;
+
+  if (startDate && endDate) {
+    startDate = new Date(startDate);
+    endDate = new Date(endDate);
+    startDate.setUTCHours(0, 0, 0, 0);
+    endDate.setUTCHours(0, 0, 0, 0);
+
+    const query = `
+      SELECT
+          SUM(op.total_price) AS total_revenew
+          FROM orders o
+          LEFT JOIN (
+              SELECT op.order_id, SUM(op.price) AS total_price
+                      FROM orders_products op
+                  GROUP BY op.order_id
+              ) op ON o.id = op.order_id
+          WHERE DATE(o.created_at) BETWEEN $1 AND $2
+  `;
+
+    const totalRevenue = await pool.query(query, [
+      startDate.toISOString(),
+      endDate.toISOString(),
+    ]);
+
+    if (totalRevenue.rowCount === 0) {
+      return res.status(404).send("No orders found for the date");
+    }
+
+    return res.send(totalRevenue.rows);
+  }
+
+  const query = `
+      SELECT
+          SUM(op.total_price) AS total_revenew
+          FROM orders o
+          LEFT JOIN (
+              SELECT op.order_id, SUM(op.price) AS total_price
+                      FROM orders_products op
+                  GROUP BY op.order_id
+              ) op ON o.id = op.order_id
+  `;
+
+  const totalRevenue = await pool.query(query);
+
+  if (totalRevenue.rowCount === 0) {
+    return res.status(404).send("No orders found");
+  }
+
+  return res.send(totalRevenue.rows);
+};
+
+
+exports.topSoldProducts = async (req, res) => {
+  try {
+    const query = `
+      SELECT
+        p.id AS product_id,
+        p.name AS product_name,
+        SUM(op.quantity) AS total_sold_quantity
+      FROM products p
+      LEFT JOIN orders_products op ON p.id = op.product_id
+      GROUP BY p.id, p.name
+      HAVING SUM(op.quantity) > 0  -- Exclude products with no sales
+      ORDER BY total_sold_quantity DESC  -- Order by total sold quantity in descending order
+      LIMIT 10
+    `;
+
+    const topSellingProduct = await pool.query(query);
+
+    if (topSellingProduct.rowCount === 0) {
+      return res.status(404).json({ message: "No products found or no sales data available" });
+    }
+
+    return res.status(200).json(topSellingProduct.rows);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
+exports.getTotalProductSales = async (req, res) => {
+  const { startDate, endDate } = req.query;
+
+  try {
+    const query = `
+      SELECT
+        p.id AS product_id,
+        p.name AS product_name,
+        COUNT(op.id) AS product_count
+      FROM orders o
+      JOIN orders_products op ON o.id = op.order_id
+      JOIN products p ON op.product_id = p.id
+      WHERE DATE(o.created_at) BETWEEN $1 AND $2
+      GROUP BY p.id, p.name
+    `;
+
+    const soldProducts = await pool.query(query, [startDate, endDate]);
+
+    if (soldProducts.rowCount === 0) {
+      return res.status(404).json({ message: "No sales data available within the specified date range" });
+    }
+
+    return res.status(200).json(soldProducts.rows);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
+
 
 
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const {
   addToCart,
-  removeFromCart
+  removeFromCart,
+  checkout,
+  totalRevenue,
+  getAllOrders,
+  viewOrderOfUser,
+  topSoldProducts,
+  getTotalProductSales
 } = require("../modules/order/orderController");
 
 
@@ -73,22 +79,81 @@ const {
 
 /**
  * @swagger
- * /orders/:
+ * /orders/removeFromCart:
+ *   delete:
+ *     summary: Remove a product from the shopping cart
+ *     tags:
+ *       - Order
+ *     parameters:
+ *       - in: query
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user who owns the cart.
+ *       - in: query
+ *         name: productId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the product to be removed from the cart.
+ *     responses:
+ *       '200':
+ *         description: Product removed from the cart successfully
+ *       '400':
+ *         description: Bad request. Invalid input data.
+ *       '404':
+ *         description: Not found. The specified product or cart does not exist.
+ *       '500':
+ *         description: Internal Server Error. An error occurred on the server.
+ */
+
+/**
+ * @swagger
+ * /orders/checkout:
+ *   post:
+ *     summary: Checkout a shopping cart
+ *     tags:
+ *       - Order
+ *     parameters:
+ *       - in: query
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user who owns the cart to be checked out.
+ *       - in: query
+ *         name: cartId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the shopping cart to be checked out.
+ *     responses:
+ *       '200':
+ *         description: Checkout successful
+ *       '404':
+ *         description: User or cart not found
+ *       '500':
+ *         description: Internal server error
+ */
+
+
+
+/**
+ * @swagger
+ * /orders:
  *   get:
- *     summary: Get a list of all shopping orders
+ *     summary: Get all orders with product details
+ *     description: Retrieve a list of all orders along with their associated product details.
  *     tags:
  *       - Order
  *     responses:
  *       '200':
- *         description: List of shopping orders retrieved successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/orders'
+ *         description: List of all orders with product details retrieved successfully.
+ *       '404':
+ *         description: No orders found.
  *       '500':
- *         description: Internal server error
+ *         description: Internal server error.
  */
 
 /**
@@ -120,35 +185,95 @@ const {
 
 /**
  * @swagger
- * /orders/removeFromCart:
- *   delete:
- *     summary: Remove a product from the shopping cart
+ * /orders/TotalRevenue:
+ *   get:
+ *     summary: Get total revenue
+ *     description: Retrive total revenue in the interval between.
  *     tags:
  *       - Order
  *     parameters:
  *       - in: query
- *         name: userId
- *         required: true
+ *         name: startDate
  *         schema:
  *           type: string
- *         description: The ID of the user who owns the cart.
+ *           format: date
+ *         description: Start date for the range (YYYY-MM-DD).
  *       - in: query
- *         name: productId
- *         required: true
+ *         name: endDate
  *         schema:
  *           type: string
- *         description: The ID of the product to be removed from the cart.
+ *           format: date
+ *         description: End date for the range (YYYY-MM-DD).
  *     responses:
  *       '200':
- *         description: Product removed from the cart successfully
+ *         description: Data within the date range.
  *       '400':
- *         description: Bad request. Invalid input data.
- *       '404':
- *         description: Not found. The specified product or cart does not exist.
- *       '500':
- *         description: Internal Server Error. An error occurred on the server.
+ *         description: Invalid input data.
  */
 
+/**
+ * @swagger
+ * /orders/getTopTenSellingProduct:
+ *   get:
+ *     summary: Get the top 10 most sold products
+ *     description: Retrieve the top 10 most sold products based on order history.
+ *     tags:
+ *       - Order
+ *     responses:
+ *       '200':
+ *         description: List of the top 10 most sold products retrieved successfully.
+ *       '404':
+ *         description: No products found or no sales data available.
+ *       '500':
+ *         description: Internal server error.
+ */
+
+/**
+ * @swagger
+ * /orders/totalProductSales:
+ *   get:
+ *     summary: Get the total number of product sales within a date range
+ *     description: Retrieve the total number of product sales within a specified date range.
+ *     tags:
+ *       - Order
+ *     parameters:
+ *       - in: query
+ *         name: startDate
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: date
+ *         description: The start date of the date range for sales data (e.g., 'YYYY-MM-DD').
+ *       - in: query
+ *         name: endDate
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: date
+ *         description: The end date of the date range for sales data (e.g., 'YYYY-MM-DD').
+ *     responses:
+ *       '200':
+ *         description: Total number of product sales within the specified date range retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 total_sales:
+ *                   type: integer
+ *                   description: The total number of product sales within the specified date range.
+ *       '404':
+ *         description: No sales data available within the specified date range.
+ *       '500':
+ *         description: Internal server error.
+ */
+
+router.route("/totalProductSales").get(getTotalProductSales)
+router.route("/getTopTenSellingProduct").get(topSoldProducts)
 router.route("/removeFromCart").delete(removeFromCart)
 router.route("/addtocart").post(addToCart);
+router.route("/checkout").post(checkout)
+router.route("/totalRevenue").get(totalRevenue)
+router.route("/").get(getAllOrders)
+router.route("/:id").get(viewOrderOfUser)
  module.exports = router;
